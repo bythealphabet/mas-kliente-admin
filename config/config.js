f08728c466f502aa922e9dc22adb771bd3006fbe
bythealphabet <bythealphabet@gmail.com>
2021-07-26 22:02:32 -0400

    adding nodemailer to the app and it works fine

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "your_secret_key",
+  jwtAcountActivation:
+    process.env.JWT_ACCOUNT_ACTIVATION || "acount_activation",
   mongoUri:
     process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
